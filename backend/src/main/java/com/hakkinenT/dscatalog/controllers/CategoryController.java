6c4e49c1f0ac1672b36b0f240fe2e3b52b7ef7e9
Tawanna Hakkinen <thol_16@hotmail.com>
2023-11-10 22:14:48 -0300

    GET by category id created in Service and Controller classes

--- backend/src/main/java/com/hakkinenT/dscatalog/controllers/CategoryController.java
+++ backend/src/main/java/com/hakkinenT/dscatalog/controllers/CategoryController.java
@@ -7,6 +7,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,5 +23,9 @@ public class CategoryController {
         return ResponseEntity.ok(categories);
     }
 
-
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<CategoryDTO> findById(@PathVariable Long id){
+        CategoryDTO categoryDTO = categoryService.findById(id);
+        return ResponseEntity.ok(categoryDTO);
+    }
 }
--- backend/src/main/java/com/hakkinenT/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/hakkinenT/dscatalog/services/CategoryService.java
@@ -3,10 +3,12 @@ package com.hakkinenT.dscatalog.services;
 import com.hakkinenT.dscatalog.dto.CategoryDTO;
 import com.hakkinenT.dscatalog.entities.Category;
 import com.hakkinenT.dscatalog.repositories.CategoryRepository;
+import com.hakkinenT.dscatalog.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+
 
 @Service
 public class CategoryService {
@@ -16,5 +18,11 @@ public class CategoryService {
     public Page<CategoryDTO> findAll(Pageable pageable){
         Page<Category> categories = categoryRepository.findAll(pageable);
         return categories.map(cat -> new CategoryDTO(cat));
+    }
+
+    public CategoryDTO findById(Long id){
+        Category category = categoryRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Recurso não encontrado!"));
+        return new CategoryDTO(category);
     }
 }
