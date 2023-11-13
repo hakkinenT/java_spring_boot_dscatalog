93732c708a4a760dcdb1c6af9262dd18c19b216c
Tawanna Hakkinen <thol_16@hotmail.com>
2023-11-13 20:39:04 -0300

    DELETE category method created in Service and Controller classes

--- backend/src/main/java/com/hakkinenT/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/hakkinenT/dscatalog/services/CategoryService.java
@@ -3,12 +3,15 @@ package com.hakkinenT.dscatalog.services;
 import com.hakkinenT.dscatalog.dto.CategoryDTO;
 import com.hakkinenT.dscatalog.entities.Category;
 import com.hakkinenT.dscatalog.repositories.CategoryRepository;
+import com.hakkinenT.dscatalog.services.exceptions.DatabaseException;
 import com.hakkinenT.dscatalog.services.exceptions.ResourceNotFoundException;
 import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 
@@ -49,6 +52,19 @@ public class CategoryService {
             return new CategoryDTO(category);
         }catch (EntityNotFoundException e){
             throw new ResourceNotFoundException("Recurso não encontrado");
+        }
+    }
+
+    @Transactional(propagation = Propagation.SUPPORTS)
+    public void delete(Long id){
+        if(!categoryRepository.existsById(id)){
+            throw new ResourceNotFoundException("Recurso não encontrado!");
+        }
+
+        try {
+            categoryRepository.deleteById(id);
+        }catch (DataIntegrityViolationException e){
+            throw new DatabaseException("Violação de Integridade");
         }
     }
 }
