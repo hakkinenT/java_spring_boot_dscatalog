4706ccec3f7fc8499e0f6d9a88b7e815e85fc01f
Tawanna Hakkinen <thol_16@hotmail.com>
2023-11-13 20:20:19 -0300

    PUT category method created in Service and Controller classes

--- backend/src/main/java/com/hakkinenT/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/hakkinenT/dscatalog/services/CategoryService.java
@@ -4,10 +4,12 @@ import com.hakkinenT.dscatalog.dto.CategoryDTO;
 import com.hakkinenT.dscatalog.entities.Category;
 import com.hakkinenT.dscatalog.repositories.CategoryRepository;
 import com.hakkinenT.dscatalog.services.exceptions.ResourceNotFoundException;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 
 @Service
@@ -25,12 +27,25 @@ public class CategoryService {
                 .orElseThrow(() -> new ResourceNotFoundException("Recurso não encontrado!"));
         return new CategoryDTO(category);
     }
-
+    
     public CategoryDTO insert(CategoryDTO dto){
         Category category = new Category();
         category.setName(dto.getName());
 
         category = categoryRepository.save(category);
         return new CategoryDTO(category);
+    }
+
+    @Transactional
+    public CategoryDTO update(Long id,CategoryDTO dto){
+        try {
+            Category category = categoryRepository.getReferenceById(id);
+            category.setName(dto.getName());
+
+            category = categoryRepository.save(category);
+            return new CategoryDTO(category);
+        }catch (EntityNotFoundException e){
+            throw new ResourceNotFoundException("Recurso não encontrado");
+        }
     }
 }
