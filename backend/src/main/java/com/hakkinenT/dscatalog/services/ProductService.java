49ff4cf3194149fe4c049dd63363d7fed09c92ea
Tawanna Hakkinen <thol_16@hotmail.com>
2023-11-13 21:54:37 -0300

    fix: Transactional annotation added to findAll method

--- backend/src/main/java/com/hakkinenT/dscatalog/services/ProductService.java
+++ backend/src/main/java/com/hakkinenT/dscatalog/services/ProductService.java
@@ -7,14 +7,17 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class ProductService {
     @Autowired
     private ProductRepository productRepository;
 
+    @Transactional(readOnly = true)
     public Page<ProductDTO> findAll(Pageable pageable){
         Page<Product> products = productRepository.findAll(pageable);
         return products.map(product -> new ProductDTO(product));
     }
+
 }
