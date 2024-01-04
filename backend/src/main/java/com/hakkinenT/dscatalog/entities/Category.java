0ada8b942a50f83d6b213ea6727e74a14a96be68
Tawanna <thol_16@hotmail.com>
2024-01-03 21:17:02 -0300

    ORM mapping, database seeding

--- backend/src/main/java/com/hakkinenT/dscatalog/entities/Category.java
+++ backend/src/main/java/com/hakkinenT/dscatalog/entities/Category.java
@@ -3,7 +3,9 @@ package com.hakkinenT.dscatalog.entities;
 import jakarta.persistence.*;
 
 import java.time.Instant;
+import java.util.HashSet;
 import java.util.Objects;
+import java.util.Set;
 
 @Entity
 @Table(name = "tb_category")
@@ -19,6 +21,9 @@ public class Category {
 
     @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
     private Instant updatedAt;
+
+    @ManyToMany(mappedBy = "categories")
+    private Set<Product> products = new HashSet<>();
 
     public Category() {
     }
@@ -42,6 +47,10 @@ public class Category {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public Set<Product> getProducts() {
+        return products;
     }
 
     public Instant getCreatedAt() {
