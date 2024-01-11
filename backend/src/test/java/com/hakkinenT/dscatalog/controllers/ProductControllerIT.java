7e7560487069b6bd6f3a2eee0c8259062e47ba96
Tawanna <thol_16@hotmail.com>
2024-01-10 21:21:15 -0300

    ajustes nos testes

--- backend/src/test/java/com/hakkinenT/dscatalog/controllers/ProductControllerIT.java
+++ backend/src/test/java/com/hakkinenT/dscatalog/controllers/ProductControllerIT.java
@@ -3,6 +3,7 @@ package com.hakkinenT.dscatalog.controllers;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.hakkinenT.dscatalog.dto.ProductDTO;
 import com.hakkinenT.dscatalog.tests.Factory;
+import com.hakkinenT.dscatalog.tests.TokenUtil;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -29,15 +30,25 @@ public class ProductControllerIT {
     @Autowired
     private ObjectMapper mapper;
 
+    @Autowired
+    private TokenUtil tokenUtil;
+
     private Long existingId;
     private Long nonExistingId;
     private Long countTotalProducts;
+
+    private String username, password, bearerToken;
 
     @BeforeEach
     void setUp() throws Exception{
         existingId = 1L;
         nonExistingId = 1000L;
         countTotalProducts = 25L;
+
+        username = "maria@gmail.com";
+        password = "123456";
+
+        bearerToken = tokenUtil.obtainAccessToken(mockMvc, username, password);
     }
 
     @Test
@@ -64,6 +75,7 @@ public class ProductControllerIT {
 
         ResultActions result = mockMvc.perform(
                 put("/products/{id}", existingId)
+                        .header("Authorization", "Bearer " + bearerToken)
                         .content(jsonBody)
                         .contentType(MediaType.APPLICATION_JSON)
                         .accept(MediaType.APPLICATION_JSON)
@@ -81,6 +93,7 @@ public class ProductControllerIT {
 
         ResultActions result = mockMvc.perform(
                 put("/products/{id}", nonExistingId)
+                        .header("Authorization", "Bearer " + bearerToken)
                         .content(jsonBody)
                         .contentType(MediaType.APPLICATION_JSON)
                         .accept(MediaType.APPLICATION_JSON)
